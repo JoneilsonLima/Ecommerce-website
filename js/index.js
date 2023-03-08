2b20ef2684329f3f2d40ae82685e878e568b5bf2
joneilson <joneilson84@gmail.com>
2023-03-07 21:10:48 -0300

    feat: add notification toast

--- js/index.js
+++ js/index.js
@@ -11,3 +11,12 @@ const modalCloseFunc = () => modal.classList.add('closed');
 // MODAL EVENTLISTENER
 modalCloseOverlay.addEventListener('click', modalCloseFunc);
 modalCloseBtn.addEventListener('click', modalCloseFunc);
+
+
+
+// NOTIFICATION TOAST VARIABLES
+const notificationToast = document.querySelector('[data-toast]');
+const toastCloseBtn = document.querySelector('[data-toast-close]');
+
+// NOTIFICATION TOAST EVENTLISTENER
+toastCloseBtn.addEventListener('click', () => notificationToast.classList.add('closed'));
